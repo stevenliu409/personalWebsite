838589e3412811682cdbd41337269db201614619
Steven Liu <stevenliu@MacBook.local>
2016-03-31 12:38:54 -0700

    added copyright footer and year

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ $(document).ready(function() {
     (function blink(){
         $(".intro-header-text-cursor").fadeOut(500).fadeIn(500, blink);
     })();
+
+    $('#copyright').text(new Date().getFullYear());
+
 });
